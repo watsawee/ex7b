4b3c55eff599e36faa7e9ffecfb8efe8bc06e6b6
Sitthiporn <2020511101002@live4.utcc.ac.th>
2022-03-16 19:20:52 +0700

    Bug Fix#1 - Adding required attribute

--- validate.js
+++ validate.js
@@ -1,3 +1,19 @@
+function checkfname() {
+  let fname = (document.getElementById("fname").value).trim();
+  if (fname.length != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function checklname() {
+  let lname = (document.getElementById("lname").value).trim();
+  if (lname.length != 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
   if (sid.length == 10) {
@@ -17,6 +33,16 @@ function checkCandiNo() {
 }
 
 function validateForm(){
+	if(!checklname()){
+	  alert("Invalid value for Student Last Name!");
+	  document.getElementById("lname").focus();
+	  return false;
+	}
+	if(!checkfname()){
+	  alert("Invalid value for Student Name!");
+	  document.getElementById("fname").focus();
+	  return false;
+	}
 	if(!checkSID()){
 	  alert("Invalid value for Student ID!");
 	  document.getElementById("sid").focus();
