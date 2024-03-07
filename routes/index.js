33eff4cc405d2d3e52112dfbd6cd0ed84152dbb2
Chakkaphob <2210511101015@live4.utcc.ac.th>
2024-03-07 14:59:24 +0700

    add readline function

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+ 
+function file_readline(){
+  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/wordcnt', function (req, res) {
+router.get('/wordcnt/:fav_team', function (req, res) {
   let fav_team = req.params.fav_team;
-  res.send("word count API"+fav_team);	
+//-----------------
+  file_readline();
+//-----------------
+  res.send("word count API-"+fav_team);	
 });
 
 module.exports = router;
