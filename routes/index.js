0ae47e4391fbeffdebf8f76999efcbf694fcc5bd
Watsawee <watsawee@gmail.com>
2024-03-07 14:59:11 +0700

    add readline function

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+ 
+function file_readline(){
+  const contents = fs.readFileSync('public\\input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +16,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/wordcnt/:fav_team', function (req, res) {
   let fav_team = req.params.fav_team;
+  //----------------
+  file_readline();
+  //----------------
   res.send("word count API - "+fav_team);	
 });
 
