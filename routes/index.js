a30fd60cc120a91a5689f867d22353077c8bae75
sunkittitat <tanapattanalux@gmail.com>
2024-03-07 14:59:21 +0700

    add readline function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+ 
+function file_readline(){
+  const contents = fs.readFileSync('public/input.txt', 'utf-8');
+  contents.split(/\r?\n/).forEach(line =>  {
+    console.log(`Line from file: ${line}`);
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,6 +17,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/wordcnt/:fav_team', function (req, res) {
   let fav_team = req.params.fav_team;
+  //--------
+  file_readline();
+  //--------
   res.send("word count API - "+fav_team);	
 });
 
