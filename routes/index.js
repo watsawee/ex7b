ce56a5a2d5f2cf8c5d09b7b2f3bd4631fcb29a18
Chakkaphob <2210511101015@live4.utcc.ac.th>
2024-03-07 15:27:50 +0700

    add code to display the result

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,14 @@ router.get('/wordcnt/:fav_team', function (req, res) {
   let fav_team = req.params.fav_team;
 //-----------------
   file_readline();
-//-----------------
-  res.send("word count API-"+fav_team);	
- //------------------
- let cnt = file_readline(fav_team);
-  console.log("word count -> " +cnt);
+//------------------
+let cnt = file_readline(fav_team);
+console.log("word count -> " +cnt);
 //-------------------
+let heading = "<h2>Word Count Result</h2>"
+let str1 = "Your favorite team -> <b>"+fav_team+"</b><br>";
+let str2 = "Number of occurrences -> <b>"+cnt+"</b><br>";
+res.send(heading+str1+str2);
 });
 
 module.exports = router;
